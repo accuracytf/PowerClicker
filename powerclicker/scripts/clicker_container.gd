baeb1784e53e32219dbe79512a13acb43ba3c73c
accuracytf <miltonhansson1@gmail.com>
2024-10-09 22:02:32 +0200

    lite progressbar saker
    det må finnas ett error just nu

--- powerclicker/scripts/clicker_container.gd
+++ powerclicker/scripts/clicker_container.gd
@@ -6,7 +6,11 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 var clicked = false
-var amount = 1;
+var amount = 1
+var progress_bar = get_node("../Top_container/ProgressBar")
+progress_bar.min_value = 0
+progress_bar.max_value = 100
+progress_bar.value = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var labeltext = get_node("../Left_Container/Money")
@@ -14,6 +18,8 @@ func _process(delta: float) -> void:
 		Globals.Money += 1
 		labeltext.text = "Money: " + str(Globals.Money)
 		clicked = false
+		progress_bar.value += 1
+		
 
 	
 	pass
