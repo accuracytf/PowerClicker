64e7f912e0c129bec08e43d44f72b020c75d938c
Henrik Hellström <c24hhm@cs.umu.se>
2024-10-10 11:46:25 +0200

    mer i shoppen + bilder

--- powerclicker/scripts/shop.gd
+++ powerclicker/scripts/shop.gd
@@ -1,6 +1,19 @@
 extends Control
 
+class shopItem:
+	var price: int
+	var name: String
+	var output: float
+	var CO2_output: float
+	var scale: float = 1.5
+	var label: Label
 
+var solar: shopItem
+var wind: shopItem
+var hydro: shopItem
+var coal: shopItem
+var gas: shopItem
+var oil: shopItem
 
 func _ready() -> void:
 	pass # Replace with function body.
