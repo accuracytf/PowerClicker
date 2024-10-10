9df2c2e5df48cb2669b2a560bb798435e45b3847
accuracytf <miltonhansson1@gmail.com>
2024-10-10 09:04:17 +0200

    mini fix

--- powerclicker/scripts/clicker_container.gd
+++ powerclicker/scripts/clicker_container.gd
@@ -1,24 +1,25 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 var clicked = false
 var amount = 1
-var progress_bar = get_node("../Top_container/ProgressBar")
-progress_bar.min_value = 0
-progress_bar.max_value = 100
-progress_bar.value = 0
+var progress_bar = null
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	progress_bar = get_node("../Top_container/ProgressBar")
+	progress_bar.min_value = 0
+	progress_bar.max_value = 100
+	progress_bar.value = 0
+	pass # Replace with function body.
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var labeltext = get_node("../Left_Container/Money")
 	if(clicked == true): 
 		Globals.Money += 1
 		labeltext.text = "Money: " + str(Globals.Money)
+		progress_bar.value +=5
 		clicked = false
-		progress_bar.value += 1
 		
 
 	
