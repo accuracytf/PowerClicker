b4357f38e363bb1a4efbd9f6e6085a3028fa63a1
Henrik Hellström <henrik.hellstrom@bjorkforsen.se>
2024-10-14 16:19:54 +0200

    mer på shop + ändringar i barkgrundsbilden

--- powerclicker/scripts/shop.gd
+++ powerclicker/scripts/shop.gd
@@ -1,22 +1,38 @@
 extends Control
 
 class shopItem:
-	var price: int
 	var name: String
+	var price: int
+	var amount = 0
 	var output: float
 	var CO2_output: float
-	var scale: float = 1.5
+	var priceScale: float
 	var label: Label
 
-var solar: shopItem
-var wind: shopItem
-var hydro: shopItem
-var coal: shopItem
-var gas: shopItem
-var oil: shopItem
+var solar: shopItem = shopItem.new()
+var wind: shopItem = shopItem.new()
+var hydro: shopItem = shopItem.new()
+var coal: shopItem = shopItem.new()
+var gas: shopItem = shopItem.new()
+var oil: shopItem = shopItem.new()
+
+func _initShopitem(item: shopItem, name: String, price: int, output: float, CO2_output: float, priceScale: float, label: Label):
+	item.name = name
+	item.price = price
+	item.output = output
+	item.CO2_output = CO2_output
+	item.priceScale = priceScale
+	item.label = label
+
+func _updateLabel(item: shopItem):
+	item.label.text = item.name + " " + str(item.amount)
+
 
 func _ready() -> void:
-	pass # Replace with function body.
+	
+	_initShopitem(solar, "solar", 10, 10, 10, 1.5, $ScrollContainer_Green/VBoxContainer_Green/TextureRect_Solar/Label)
+	_updateLabel(solar)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
