33c5c6d112c20575d899de0452ad6b1aa467500c
accuracytf <miltonhansson1@gmail.com>
2024-10-17 17:52:47 +0200

    lite procent saker

--- PowerClicker/powerclicker/scripts/Globals.gd
+++ PowerClicker/powerclicker/scripts/Globals.gd
@@ -3,3 +3,4 @@ extends Node
 var money : int = 0
 var kWd : float = 0
 var CO2PerYear : int = 0
+var maxenergy : float = 105681000000000.0
--- PowerClicker/powerclicker/scripts/top.gd
+++ PowerClicker/powerclicker/scripts/top.gd
@@ -1,36 +1,23 @@
 extends Control
 
 # Reference to the ProgressBar using @onready
-@onready var progress_bar = $ProgressBar 
-@onready var progress_bar1 = $ProgressBar2
-@onready var progress_bar2 = $ProgressBar3
+@onready var label = $Label 
+
 
 var timer: float = 0.0
 var duration: float = 10.0  # Duration for the progress to complete (in seconds)
+var procentage: float = 0.0
 
 func _ready() -> void:
+	label.text = "hej"
 	# Initialize the ProgressBar
-	progress_bar.min_value = 0
-	progress_bar.max_value = 100
-	progress_bar.value = 0
-	progress_bar1.min_value = 0
-	progress_bar1.max_value = 100
-	progress_bar1.value = 0
-	progress_bar2.min_value = 0
-	progress_bar2.max_value = 100
-	progress_bar2.value = 0
+
 
 func _process(delta: float) -> void:
 	# Update the timer
 	timer += delta
+	if(timer>=1): 
+		procentage += Globals.kWd/Globals.maxenergy
+		label.text = "You have produced \n" + str(procentage) + "\nof the energy requiered for the whole world during 1 year";
+		timer =- 1
 	
-	# Calculate the current progress percentage
-	if progress_bar.value < 100:
-		var progress_percentage =+ Globals.kWd /10000
-		progress_bar.value = progress_percentage  # Update the ProgressBar value
-		progress_bar1.value = progress_percentage  
-		progress_bar2.value = progress_percentage  
-	else:
-		progress_bar.value = 100  # Set to maximum when done
-		progress_bar1.value = 100  
-		progress_bar2.value = 100  
