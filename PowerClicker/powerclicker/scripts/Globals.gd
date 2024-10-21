d44620318f1b19bf384b15de36f69070a3a589c6
Henrik Hellström <henrik.hellstrom@bjorkforsen.se>
2024-10-21 12:53:31 +0200

    Animation på hamstern

--- PowerClicker/powerclicker/scripts/Globals.gd
+++ PowerClicker/powerclicker/scripts/Globals.gd
@@ -2,5 +2,5 @@ extends Node
 
 var money : int = 0
 var kWd : float = 0
-var CO2PerYear : int = 0
+var CO2PerYear : float = 0
 var maxenergy : float = 105681000000000.0
--- PowerClicker/powerclicker/scripts/clicker.gd
+++ PowerClicker/powerclicker/scripts/clicker.gd
@@ -1,13 +1,35 @@
 extends Control
 
+var clicks: int = 0
+var seconds_passed: float = 0
+var animation: AnimatedSprite2D
+var wheel: Control
+var wheel_rotation: float = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation = get_parent().get_node("Hamter_animation")
+	wheel = get_parent().get_node("Clicker")
+	
+	animation.play("run")
+	animation.set_speed_scale(0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	seconds_passed += delta  # Increment seconds by the time since the last frame
+	if seconds_passed >= 0.25:
+		seconds_passed -= 0.25
+		animation.set_speed_scale(sqrt(clicks))
+		
+		wheel_rotation += clicks
+		if wheel_rotation > 36:
+			wheel_rotation -= 36
+		print(wheel_rotation)
+		wheel.set_rotation_degrees(wheel_rotation*10)
+		
+		
+		clicks = 0
 
 func _on_click() -> void:
 	var labeltext = get_node("../../Left_Container/Money")
@@ -15,3 +37,4 @@ func _on_click() -> void:
 	labeltext.text = "Money: " + str(Globals.money)
 	var click_sound = get_node("ClickSound")
 	click_sound.play()
+	clicks += 1
