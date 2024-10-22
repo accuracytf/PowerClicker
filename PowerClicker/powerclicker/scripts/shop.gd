af105756e7c0e3d9ac9d79c33d12e4b3e356baf5
Henrik Hellström <henrik.hellstrom@bjorkforsen.se>
2024-10-22 21:18:26 +0200

    Buy Max

--- PowerClicker/powerclicker/scripts/shop.gd
+++ PowerClicker/powerclicker/scripts/shop.gd
@@ -18,6 +18,8 @@ var gas: shopItem = shopItem.new()
 var oil: shopItem = shopItem.new()
 var buildings = [solar, wind, hydro, coal, gas, oil]
 
+var buyOne : bool = true
+
 func _initShopitem(item: shopItem, name: String, price: int, output: float, CO2_output: float, priceScale: float, container: TextureRect):
 	item.name = name
 	item.price = price
@@ -31,6 +33,56 @@ func _updateLabel(item: shopItem):
 	item.container.get_node("Label").text = item.name + " " + str(item.amount)
 	item.container.get_node("Button").text = Globals.suffix(ceil(item.price)) + "kr"
 
+func _buyOne(building: shopItem, playSound: bool = true): 
+	if (Globals.money >= building.price):
+		if playSound:
+			var sound = get_node("SuccessSound")
+			sound.play()
+		Globals.money -= building.price
+		if building.name == "solar" or building.name == "Wind Turbines" or building.name == "Hydroelectric":
+			Globals.greenkWd += building.output
+		Globals.kWd += building.output
+		Globals.CO2PerDay += building.CO2_output
+		building.price += building.priceScale * building.startPrice
+		building.amount += 1
+		_updateLabel(building)
+	else:
+		var sound = get_node("ErrorSound")
+		sound.play()
+
+func _buyMax(building: shopItem):
+	var m : float = building.price
+	var k : float = building.startPrice * building.priceScale
+	var sum : float = Globals.money
+	
+	var a : float = k
+	var b : float = 2 * m+k
+	var c : float = 2 * m - 2 * sum
+	
+	print(a)
+	print(b)
+	print(c)
+	
+	print(sqrt(pow(b, 2) - 4 * a * c))
+	
+	var x : int = floor((-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a)) + 1
+	
+	if  x == 0:
+		var sound = get_node("ErrorSound")
+		sound.play()
+		return
+	
+	var totalPrice : float = (x) * (2 * m + k * (x - 1)) / 2
+	
+	Globals.money -= totalPrice
+	if building.name == "solar" or building.name == "Wind Turbines" or building.name == "Hydroelectric":
+		Globals.greenkWd += building.output * x
+	Globals.kWd += building.output * x
+	Globals.CO2PerDay += building.CO2_output * x
+	building.price += building.priceScale * building.startPrice * x
+	building.amount += 1 * x
+	_updateLabel(building)
+	
 
 func _ready() -> void:
 	
@@ -49,23 +101,13 @@ func _ready() -> void:
 
 func _buy_button_pressed(building: shopItem):
 	
-	if (Globals.money >= building.price):
-		var sound = get_node("SuccessSound")
-		sound.play()
-		Globals.money -= building.price
-		if building.name == "solar" or building.name == "Wind Turbines" or building.name == "Hydroelectric":
-			Globals.greenkWd += building.output
-		Globals.kWd += building.output
-		Globals.CO2PerDay += building.CO2_output
-		building.price += building.priceScale * building.startPrice
-		building.amount += 1
-		_updateLabel(building)
+	if buyOne:
+		_buyOne(building)
 	else:
-		var sound = get_node("ErrorSound")
-		sound.play()
+		_buyMax(building)
+
 
 func _sell_button_pressed(building: shopItem):
-	
 	if (building.amount > 0):
 		var sound = get_node("SuccessSound") ## Behöver nytt ljud för att sälja något
 		sound.play()
@@ -79,4 +121,12 @@ func _sell_button_pressed(building: shopItem):
 	else:
 		var sound = get_node("ErrorSound")
 		sound.play()
-	
+
+
+
+func _on_buy_max_pressed() -> void:
+	buyOne = false
+
+
+func _on_buy_one_pressed() -> void:
+	buyOne = true
